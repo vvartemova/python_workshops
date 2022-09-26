22acb74ffc6417b197f780e02fe6c7fe36a7435a
Victoria <vvartemova@yandex.ru>
2022-09-26 17:04:04 +0300

    Lesson5

---
+++ Lesson5/task4.py
@@ -0,0 +1,36 @@
+#Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+#Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+data = open ('unput.txt','r')
+for line in data:
+    print(line)
+    b = line
+data.close()
+final = open ('output.txt','a')
+final.writelines(coding(b)+'\n')
+final.writelines(decoding(coding(b))+'\n')
+final.close()
